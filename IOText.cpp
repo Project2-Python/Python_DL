19d626e3a8e917584aef990363b21059ad0ea3de
HilmanPermana <96397184+HilmanPermana@users.noreply.github.com>
2022-05-09 10:44:55 +0700

    Update IOText.cpp

--- IOText.cpp
+++ IOText.cpp
@@ -1,43 +1,190 @@
-#ifndef IOTEXT_H
-#define IOTEXT_H
-#include <stdio.h>
-#include <windows.h>
-#include <conio.h>
+#include "IOText.h"
 
 
-typedef struct node* address;
-typedef struct node
-{
-	char data;
-	address left;
-	address right;
-	address up;
-	address down;
-	address linehead;
+address Alokasi(){
 	
-}TextEditor;
-
-struct NotePad{
+	address AlokasiNode;
 	
-	address head_of_notepad;
-	address cursor;
-	HANDLE hstdout;
-	COORD destcord;
+	AlokasiNode = (address)malloc(sizeof(TextEditor));
+	if(AlokasiNode != NULL){
+		
+		AlokasiNode->data = '\0';
+		AlokasiNode->right = NULL;
+		AlokasiNode->left = NULL;
+		AlokasiNode->up = NULL;
+		AlokasiNode->down = NULL;
+		AlokasiNode->linehead = NULL;
+		
+	} else {
+		
+		return 0;
+	}
 	
-};
+	return AlokasiNode;
+	
+}
 
-static struct NotePad editor;
+void Inisialisasi(){
+	
+	editor.head_of_notepad = NULL;
+	editor.cursor = NULL;
+	editor.destcord.X = 0;
+	editor.destcord.Y = 0;
+	
+}
 
+void Insertion(int data){
+	
+	address ptr = Alokasi();
+		if (data == '\r')
+			ptr->data = '\n';
+		else
+			ptr->data = data;
+			
+		if (editor.head_of_notepad == NULL)
+		{
+			address temp = Alokasi();
+			temp->data = '\0';
+			temp->linehead = temp;
+			ptr->linehead = temp;
+			temp->right = ptr;
+			ptr->left = temp;
+			editor.head_of_notepad = temp;
+			editor.cursor = ptr;
+		}
 
+		else
+		{
+			address temp = editor.head_of_notepad;
+			while (temp->right != NULL)
+			{
+				temp = temp->right;
+			}
+			if (data != '\r')
+			{
+				ptr->left = temp;
+				temp->right = ptr;
+				ptr->linehead = ptr->left->linehead;
+			}
+			else
+			{
+				ptr->data = '\n';
+				ptr->linehead = ptr;
+				ptr->left = temp;
+				temp->right = ptr;
+			}
+			editor.cursor = ptr;
+		}
+}
 
-//Procedure And Function
-address Alokasi();
-void Inisialisasi();
-void Insertion(int data);
-void Print_Text();
-void setCursor();
-void keyProsess();
-void Deletion();
-void swap(char tempfordel1, char tempfordel2);
+void Print_Text()
+	{
+		address ptr = editor.head_of_notepad;
+		while (ptr != NULL)
+		{
+			if (ptr->data == '\0')
+				printf("");
+			else
+			{
+				printf("%c",ptr->data);
+			}
+				
+			ptr = ptr->right;
+		}
+}
+	
+void setCursor()
+	{
+		address ptr = editor.head_of_notepad->right;
+			editor.destcord.X = 0;
+			editor.destcord.Y = 0;
+		while (ptr != NULL)
+		{
+			if (ptr->data == '\n')
+			{
+				editor.destcord.X = 0;
+				editor.destcord.Y++;
+			}
+			else
+			{
+				editor.destcord.X++;
+			}
+			if (ptr == editor.cursor)
+				return;
+			ptr = ptr->right;
+		}
+	}
 
-#endif
+void keyProsess(){
+	 char data;
+	
+	 editor.hstdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	 Inisialisasi(); 
+	
+	 while(1){
+	  SetConsoleCursorPosition(editor.hstdout, editor.destcord);
+	  data = getch();
+	  if( data == '\b'){
+	  	Deletion();
+	  	system("cls");
+	  	Print_Text();
+	  	
+	  } else {
+	  	
+	  Insertion(data);
+		system("cls");
+		Print_Text();
+	  }
+		setCursor();
+	 }
+}
+
+void Deletion()
+	{
+		address tempfordel = editor.cursor;
+		if (editor.cursor == editor.head_of_notepad)
+			return;
+		if (tempfordel->right == NULL)
+		{
+			editor.cursor = editor.cursor->left;
+			editor.cursor->right = NULL;
+			if (tempfordel->up != NULL)
+				tempfordel->up->down = NULL;
+			tempfordel->up = NULL;
+			if (tempfordel->down != NULL)
+				tempfordel->down->up = NULL;
+			tempfordel->down = NULL;
+			delete tempfordel;
+		}
+		else if (tempfordel->right != NULL)
+		{
+			editor.cursor = editor.cursor->left;
+			while (tempfordel->right->data != '\n') 
+			{
+				swap(tempfordel->data, tempfordel->right->data);
+				tempfordel = tempfordel->right;
+				if (tempfordel->right == NULL)
+					break;
+			}
+				tempfordel->left->right = tempfordel->right;
+				if (tempfordel->right != NULL)
+					tempfordel->right->left = tempfordel->left;
+
+			if(tempfordel->up!=NULL)
+				tempfordel->up->down = NULL;
+			tempfordel->up = NULL;
+			if(tempfordel->down!=NULL)
+				tempfordel->down->up = NULL;
+			tempfordel->down = NULL;
+			delete tempfordel;
+		}
+	}
+	
+void swap(char tempfordel1, char tempfordel2){
+	
+	char temp;
+	temp = tempfordel1;
+	tempfordel1 = tempfordel2;
+	tempfordel2 = temp;
+	
+}
